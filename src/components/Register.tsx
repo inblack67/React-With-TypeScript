327e41d76d03478bf29e3f57d3ad5a55c9b02899
Aman <devproddeploy@gmail.com>
2020-07-11 23:09:42 +0530

    onChange

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useRef, useState } from 'react'
 
 interface Person{
     firstName: string;
@@ -12,19 +12,28 @@ interface Props {
     person: Person;
 }
 
+const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    console.log('stop!');
+}
+
 const Register: FC<Props> = ({ password, person: { firstName } }) => {
 
     const [count, setCount] = useState<string | number | undefined | null>(null);
 
     const [lastName, setLastName] = useState<Person>({ firstName: '' });
 
-    setCount(password);
+    // setCount(password);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const divRef = useRef<HTMLInputElement>(null);
 
     return (
-        <div>
+        <div ref={divRef}>
             <p>
                 {firstName}
             </p>
+            <input onChange={onChange} type="text" ref={inputRef}/>
         </div>
     )
 }
